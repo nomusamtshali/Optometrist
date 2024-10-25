6950660acd34eb26a86ca68dd38f71e6869444a9
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-10-25 10:37:28 +0200

    added about page

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <section className="container mx-auto px-4 py-10">
+        <h2 className="text-3xl font-bold mb-6">About Us</h2>
+        <p className="text-lg text-gray-700">We are a team of highly skilled optometrists, committed to providing the best eye care services...</p>
+      </section>
+    );
+  }
+  
